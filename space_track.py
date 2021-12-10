5a5b4d285cdf2a9ff72c224638b022625d3044f2
Gavin <gorth8@gmail.com>
2021-12-10 10:52:08 -0500

    calculate velocity and position on tle_obj, space_track now plots

--- space_track.py
+++ space_track.py
@@ -1,37 +1,184 @@
-
-import requests
-#from bs4 import BeautifulSoup
+"""
+This script requires tle_obj.py to run
+"""
 import datetime as dt
 from spacetrack import SpaceTrackClient
 import spacetrack.operators as op
 from tle_obj import TLE
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import ode
+
+
+earth_radius = 6378.0 # km
+earth_mu = 398600.0 # km^3 / s^2
+J2 = 1.08262668e-3
+
+
+def circle(r):
+    """
+    Creates a circle of radius r. Used for plotting the equator.
+    :param
+        r(float): radius of circle
+    :return
+        circle_arr(array): 3D array of circle of radius r
+    """
+    x = r * np.cos(np.linspace(-np.pi, 0, 700))
+    x_return = x[len(x): 0: -1]
+
+    y = np.sqrt(r**2 - np.multiply(x, x))
+    y_return = -np.sqrt(r ** 2 - np.multiply(x_return, x_return))
+
+    x = np.concatenate((x, x_return))
+    y = np.concatenate((y, y_return))
+    z = np.zeros(len(x))
+    circle_arr = np.vstack((x, y, z))
+    return circle_arr
+
+
+def tle_cleanup(tle):
+    """
+    Takes the two line string TLE and converts it to a list containing the data from the TLE.
+    inputs
+        tle(str): Two-line elements created using the spacetrack package from space-track.org
+
+    outputs
+        tle(list): Two-line elements in a list
+
+    Example:
+    input
+        1 25544U 98067A   21330.52589841  .00021613  00000-0  40355-3 0  9994
+        2 25544  51.6433 260.0308 0004438 278.8265 247.0955 15.48696306313759
+    output
+        ['1', '25544U', '98067A', '21330.52589841', '.00021613', '00000-0', '40355-3', '0', '9994', '2', '25544', '51.6433', '260.0308', '0004438', '278.8265', '247.0955', '15.48696306313759']
+    """
+    tle = tle.split('\n')
+    l1 = tle[0].split(' ')
+    while '' in l1:
+        l1.remove('')
+    l2 = tle[1].split(' ')
+    while '' in l2:
+        l2.remove('')
+    tle = l1 + l2
+    return tle
+
+
+def diffy_q(t, y, mu):
+    # unpacking the elements in state
+    rx, ry, rz, vx, vy, vz = y
+    r = np.array([rx, ry, rz])
+
+    # norm of radius vector
+    norm_r = np.linalg.norm(r)
+
+    # two-body acceleration
+    ax, ay, az = -r*mu/norm_r**3
+
+    r_J2 = -(3 / 2) * J2 * (earth_mu / norm_r ** 2) * ((earth_radius / norm_r) ** 2)
+    rx_J2 = r_J2 * (r[0] / norm_r) * ((5 * ((r[2] / norm_r) ** 2)) - 1)
+    ry_J2 = r_J2 * (r[1] / norm_r) * ((5 * ((r[2] / norm_r) ** 2)) - 1)
+    rz_J2 = r_J2 * (r[2] / norm_r) * ((5 * ((r[2] / norm_r) ** 2)) - 3)
+
+    ax += rx_J2
+    ay += ry_J2
+    az += rz_J2
+
+    return [vx, vy, vz, ax, ay, az]
+
 
 if __name__ == '__main__':
-    st = SpaceTrackClient('user', 'pass')
-    drange = op.inclusive_range(dt.datetime(2021, 11, 26),  dt.datetime(2021, 11, 27))
-    dt_upper = op.less_than(dt.datetime(2021, 11, 27, 12))
-    dt_lower = op.greater_than(dt.datetime(2021, 11, 26, 12))
-    dt_range = dt_lower + ',' + dt_upper
-    dt_list = [dt_lower, dt_upper]
-    dt_range = ','.join(dt_list)
-    print(dt_range)
+    # get username and password from userpass.txt file
+    with open('userpass.txt') as f:
+        contents = f.readlines()
+    user = contents[0].rstrip('\n')
+    password = contents[1]
 
-    iss_tle = st.tle(norad_cat_id=[25544], epoch=dt_range, limit=1,  format='tle')
-    print(iss_tle)
-    # clean up tle
-    iss_tle = iss_tle.replace('\n','')
-    iss_tle = iss_tle.split(' ')
-    while '' in iss_tle:
-        iss_tle.remove('')
+    st = SpaceTrackClient(user, password)
+    drange = op.inclusive_range(dt.datetime(2021, 11, 26, 12),  dt.datetime(2021, 11, 27, 12))
 
-    print(iss_tle)
+    drange_next = op.inclusive_range(dt.datetime(2021, 11, 28, 12),  dt.datetime(2021, 11, 29, 12))
+    iss_tle = st.tle(norad_cat_id=[25544], epoch=drange, limit=1,  format='tle')
+    iss_next_tle = st.tle(norad_cat_id=[25544], epoch=drange_next, limit=1,  format='tle')
+    iss_tle = tle_cleanup(iss_tle)
+    iss_next_tle = tle_cleanup(iss_next_tle)
 
+
+    """Space-Track data"""
+    # create TLE object and create arrays for plotting
     iss = TLE(iss_tle)
-    iss.true_anomaly()
-    iss.semi_major_axis()
-    iss.apogee_perigee()
-    iss.print()
+    iss_path = iss.orbit
+    current_pos = iss.pos_arr
+
+    iss_next = TLE(iss_next_tle)
+    iss_next_path = iss_next.orbit
+    next_pos = iss_next.pos_arr
+
+    equator = circle(earth_radius)
 
 
+    """Numerical data"""
+    # intial position and velocity vectors
+    r0 = [iss.pos_arr[0][0], iss.pos_arr[1][0], iss.pos_arr[2][0]]
+    v0 = [iss.vel_arr[0][0], iss.vel_arr[1][0], iss.vel_arr[2][0]]
+
+    # time span
+    tspan = 48*60*60
+
+    # time step
+    dt = 50
+
+    # total number of steps
+    n_steps = int(np.ceil(tspan/dt))
+
+    # intialize arrays
+    ys = np.zeros((n_steps, 6))
+    ts = np.zeros((n_steps, 1))
+
+    # intiial contidions
+    y0 = r0 + v0 # [rx, ry, rz, vx, vy, vz]
+    ys[0] = np.array(y0)  # sets initial values according to r0 and v0
+    step = 1 # second step because first step defined as initial contidions
+
+    # intialize solver
+    solver = ode(diffy_q)
+    solver.set_integrator('lsoda')
+    solver.set_initial_value(y0, 0)
+    solver.set_f_params(earth_mu)
+
+    # propagate orbit
+    while solver.successful() and step < n_steps:
+        solver.integrate(solver.t+dt)
+        ts[step] = solver.t
+        ys[step] = solver.y
+        step +=1
+    rs = ys[:, :3]
+    vs = ys[:, 3:]
 
 
+    """Plotting"""
+    # initialize the figure
+    fig = plt.figure()
+    ax = plt.axes(projection='3d')
+
+    # plot orbit of object and equator
+    plt.plot(iss_path[0], iss_path[1], iss_path[2], 'm-', label='ISS')
+    ax.plot(iss_next_path[0], iss_next_path[1], iss_next_path[2], 'b-', label='ISS 48 hours')
+    plt.plot(current_pos[0], current_pos[1], current_pos[2], 'mo', label='ISS current position')
+    ax.plot(rs[:, 0], rs[:, 1], rs[:, 2], 'c', label='Numerically calculated trajectory')
+    plt.plot(equator[0], equator[1], equator[2], 'r--', label='Equator')
+    plt.legend()
+
+    # plotting Earth
+    _u, _v = np.mgrid[0:2*np.pi:20j, 0:np.pi:10j]
+    _x = earth_radius * np.cos(_u) * np.sin(_v)
+    _y = earth_radius * np.sin(_u) * np.sin(_v)
+    _z = earth_radius * np.cos(_v)
+
+    # axes label
+    ax.set_xlabel(['X (km)'])
+    ax.set_ylabel(['Y (km)'])
+    ax.set_zlabel(['Z (km)'])
+    plt.legend()
+    ax.plot_surface(_x,_y,_z, cmap='Blues', label='Earth', zorder=1, alpha=0.5)
+
+    plt.show()
--- tle_obj.py
+++ tle_obj.py
@@ -10,16 +10,72 @@ def eccentric_anomaly(E, M, eccentricity):
     return E - (eccentricity*np.sin(E)) - M
 
 
+def distance(r, t, a, b):
+    """
+    :param r: The distance from the center of Earth to the object location in km
+    :param t: True anomaly
+    :param a: Semi-major axis
+    :param b: Semi-minor axis
+    :return:
+    """
+    t = np.deg2rad(t)
+    #return (((1-(((r*np.cos(t))**2)/(a**2)))*(b**2))**(0.5)) - r*np.sin(t)
+
+    return np.sqrt(((1-(((r*np.cos(t))**2)/(a**2)))*(b**2))) - r*np.sin(t)
+
+
+def ellipse(a, b):
+    x = a * np.cos(np.linspace(-np.pi, 0, 250))
+    x_return = x[len(x): 0: -1]
+
+    y = np.sqrt(((1 - ((np.multiply(x, x)) / a ** 2)) * (b ** 2)))
+    y_return = -np.sqrt(((1 - ((np.multiply(x_return, x_return)) / a ** 2)) * (b ** 2)))
+
+    x = np.concatenate((x, x_return))
+    y = np.concatenate((y, y_return))
+    z = np.zeros(len(x))
+    path_arr = np.vstack((x, y, z))
+    return path_arr
+
+
+def rotation313(arr, s, t, p):
+    s = np.deg2rad(s)
+    t = np.deg2rad(t)
+    p = np.deg2rad(p)
+    # body 3: right ascension of the ascending node
+    z1_rot = np.array(([np.cos(s), np.sin(s), 0], [-np.sin(s), np.cos(s), 0], [0, 0, 1]))
+    # body 1: inclination
+    x_rot = np.array(([1, 0, 0], [0, np.cos(t), np.sin(t)], [0, -np.sin(t), np.cos(t)]))
+    # body 3: argument of perigee
+    z2_rot = np.array(([np.cos(p), np.sin(p), 0], [-np.sin(p), np.cos(p), 0], [0, 0, 1]))
+
+    rot1 = np.matmul(z1_rot, arr)
+    rot2 = np.matmul(x_rot, rot1)
+    rot3 = np.matmul(z2_rot, rot2)
+    return rot3
+
+
 class TLE:
     def __init__(self, input):
         self.norad_cat_id = input[1]
         self.epoch = float(input[3])
-        self.inclination = float(input[10])
-        self.right_ascension = float(input[11])
-        self.eccentricity = float('0.' + input[12])
-        self.arg_perigee = float(input[13])
-        self.mean_anomaly = float(input[14])
-        self.mean_motion = float(input[15])
+        self.inclination = float(input[11])
+        self.right_ascension = float(input[12])
+        self.eccentricity = float('0.' + input[13])
+        self.arg_perigee = float(input[14])
+        self.mean_anomaly = float(input[15])
+        self.mean_motion = float(input[16])
+        self.a = self.semi_major_axis()
+        self.theta = self.true_anomaly()
+        self.apogee = self.apogee_perigee()[0]
+        self.perigee = self.apogee_perigee()[1]
+        self.b = self.semi_minor_axis()[0]
+        self.c = self.semi_minor_axis()[1]
+        self.orbit = self.orbit_path()
+        self.eta = self.specific_energy()[0]
+        self.h = self.specific_energy()[1]
+        self.pos_arr = self.object_pos()
+        self.vel_arr = self.object_vel()
 
     def true_anomaly(self):  # true anomaly in degrees
         E = fsolve(eccentric_anomaly, 2, args=(self.mean_anomaly, self.eccentricity))
@@ -42,5 +98,70 @@ class TLE:
         self.ap_peri.append(perigee)
         return self.ap_peri
 
-    def print(self):
-        print(self.eccentricity, self.a, self.ap_peri)
+    def semi_minor_axis(self):
+        perigee = self.ap_peri[1]
+        self.c = self.a - perigee
+        self.b = np.sqrt((self.a**2) - (self.c**2))
+        return self.b, self.c
+
+    def orbit_path(self):
+        """
+        Creates an ellipse based on semi-major axis and semi-minor axis
+        :param:
+            a(float): semi-major axis
+            b(float): semi-minor axis
+
+        :return:
+            path(arr): 3D array of elliptical path of an orbit
+        """
+
+        pf_orbit = ellipse(self.a, self.b)
+        pf_orbit[0] -= self.c  # move the ellipse over by the distance from the center of the ellipse to the focus because Earth is defined at being at 0,0,0
+        self.orbit = rotation313(pf_orbit, self.right_ascension, self.inclination, self.arg_perigee)
+
+        return self.orbit
+
+    def specific_energy(self):
+        perigee = self.ap_peri[1]
+        self.h = np.sqrt(self.a*earth_mu*(1-(self.eccentricity**2)))
+        self.eta = (0.5*((self.h**2)/(perigee**2))) - (earth_mu/perigee)
+        self.h = np.array([0, 0, self.h])
+        self.h = rotation313(self.h, self.right_ascension, self.inclination, self.arg_perigee)
+        return self.eta, self.h
+
+    def object_pos(self):
+        """
+        Takes in the true anomaly to determine the x and y values of the object in the orbit in the perifocal frame
+        """
+        self.pos_arr = np.zeros((3, 1))
+        r = fsolve(distance, self.a, args=(self.theta, self.a, self.b))
+        if r < 0:
+            r = -r
+        t = np.deg2rad(self.theta)
+        x = r*np.cos(t)
+        y = r*np.sin(t)
+
+        self.pos_arr[0][0] = x
+        self.pos_arr[1][0] = y
+        self.pos_arr = rotation313(self.pos_arr, self.right_ascension, self.inclination, self.arg_perigee)
+        return self.pos_arr
+
+    def object_vel(self):
+        pos_scalar = np.linalg.norm(self.pos_arr)
+        vel_scalar = np.sqrt(2*(self.eta + (earth_mu/pos_scalar)))
+        pos_mat = np.ones((3, np.shape(self.orbit)[1]))
+        pos_mat[0] *= self.pos_arr[0]
+        pos_mat[1] *= self.pos_arr[1]
+        pos_mat[2] *= self.pos_arr[2]
+        d_dist_mag = np.linalg.norm(self.orbit - pos_mat, axis=0)
+        min_idx = np.argmin(d_dist_mag)
+        d_arr = np.vstack((self.orbit[0,min_idx], self.orbit[1,min_idx], self.orbit[2,min_idx]))
+        d_arr = d_arr - self.pos_arr
+        vel_unit = d_arr / np.linalg.norm(d_arr)
+        self.vel_arr = vel_unit * vel_scalar
+
+        return self.vel_arr
+
+
+
+
